c56000d27cf3cf372297c561961bba64a0f1f11b
Ilavarasi1 <sheethal.jan@gmail.com>
2020-08-28 16:56:18 +0300

    remove .git extension

--- test_bash.bash
+++ test_bash.bash
@@ -2,7 +2,7 @@ cd /home/jovyan
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
-git clone https://github.com/balazsan/RStudio.git
+git clone https://github.com/balazsan/RStudio
 
 
 # pip install ipython --upgrade
