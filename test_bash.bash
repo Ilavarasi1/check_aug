fd4055535cce61d4324d126892db7d069f9eb975
Ilavarasi1 <sheethal.jan@gmail.com>
2020-08-28 16:49:20 +0300

    test

---
+++ test_bash.bash
@@ -0,0 +1,9 @@
+cd /home/jovyan
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
+git clone https://github.com/balazsan/RStudio.git
+
+
+# pip install ipython --upgrade
+# pip install xgboost scikit-image graphviz
