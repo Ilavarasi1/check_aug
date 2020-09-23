eb8c81cd15d5df38ce23d8d993f6009d9a9095d5
Ilavarasi1 <sheethal.jan@gmail.com>
2020-09-23 13:00:25 +0300

    he

--- test_bash.bash
+++ test_bash.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 ls -lah
-chown rstudio:rstudio RStudio
-ls -lah
 cd /home/rstudio
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
 git clone https://github.com/balazsan/RStudio
+
+chown rstudio:rstudio RStudio
+ls -lah
+
 
 
 # pip install ipython --upgrade
